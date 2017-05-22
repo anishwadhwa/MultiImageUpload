cdfd354f1699861eea2b6c891fe062df17a83841
anishwadhwa16 <anish.wadhwa16@yahoo.in>
2017-05-22 23:52:41 +0530

    S3 changes finally successful. Integrated S3 SDK into application

--- app/src/main/java/quicksell/galleryupload/MainActivity.java
+++ app/src/main/java/quicksell/galleryupload/MainActivity.java
@@ -19,6 +19,13 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferListener;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferObserver;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferState;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferType;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
 import com.google.gson.JsonObject;
 
 import java.io.File;
@@ -28,6 +35,8 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import okhttp3.MultipartBody;
+import quicksell.galleryupload.Utils.S3Constants;
+import quicksell.galleryupload.Utils.S3Utils;
 import quicksell.galleryupload.Utils.SpaceItemDecorator;
 import quicksell.galleryupload.imageupload.ProgressRequestBody;
 import quicksell.galleryupload.imageupload.RetrofitService;
@@ -49,6 +58,9 @@ public class MainActivity extends AppCompatActivity implements ProgressRequestBo
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    private TransferUtility transferUtility;
+    TransferObserver observer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,6 +70,17 @@ public class MainActivity extends AppCompatActivity implements ProgressRequestBo
         setSupportActionBar(toolbar);
         checkForPermissions();
         retrofitInterface = RetrofitService.getRetrofitClient();
+
+        transferUtility = S3Utils.getTransferUtility(this);
+        observer = transferUtility.getTransfersWithType(TransferType.UPLOAD).get(0);
+        TransferListener listener = new UploadListener();
+        // Sets listeners to in progress transfers
+        if (TransferState.WAITING.equals(observer.getState())
+                || TransferState.WAITING_FOR_NETWORK.equals(observer.getState())
+                || TransferState.IN_PROGRESS.equals(observer.getState())) {
+            observer.setTransferListener(listener);
+        }
+
     }
 
     @Override
@@ -65,6 +88,28 @@ public class MainActivity extends AppCompatActivity implements ProgressRequestBo
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    /*
+     * Begins to upload the file specified by the file path.
+     */
+    private void beginUpload(String filePath) {
+        if (filePath == null) {
+            Toast.makeText(this, "Could not find the filepath of the selected file",
+                    Toast.LENGTH_LONG).show();
+            return;
+        }
+        File file = new File(filePath);
+        TransferObserver observer = transferUtility.upload(S3Constants.BUCKET_NAME, file.getName(),
+                file);
+        /*
+         * Note that usually we set the transfer listener after initializing the
+         * transfer. However it isn't required in this sample app. The flow is
+         * click upload button -> start an activity for image selection
+         * startActivityForResult -> onActivityResult -> beginUpload -> onResume
+         * -> set listeners to in progress transfers.
+         */
+        // observer.setTransferListener(new UploadListener());
     }
 
     @Override
@@ -179,8 +224,10 @@ public class MainActivity extends AppCompatActivity implements ProgressRequestBo
 
     private void startUploadingImages() {
         for (int i =0; i < uploadImages.size(); i++) {
-            File file = new File(uploadImages.get(i).getImagePath());
-            ProgressRequestBody fileBody = new ProgressRequestBody(file, this);
+//            File file = new File(uploadImages.get(i).getImagePath());
+
+            beginUpload(uploadImages.get(i).getImagePath());
+            /*ProgressRequestBody fileBody = new ProgressRequestBody(file, this);
             MultipartBody.Part filePart = MultipartBody.Part.createFormData("image", file.getName(), fileBody);
             Call<JsonObject> request = retrofitInterface.uploadImage(filePart,"quicksell.images","uploads","abc" + i + ".jpg");
             request.enqueue(new Callback<JsonObject>() {
@@ -194,7 +241,7 @@ public class MainActivity extends AppCompatActivity implements ProgressRequestBo
                     Log.v(TAG, "upload failed :" );
                     t.printStackTrace();
                 }
-            });
+            });*/
         }
     }
 
@@ -241,4 +288,35 @@ public class MainActivity extends AppCompatActivity implements ProgressRequestBo
             }
         }
     }
+
+    private class UploadListener implements TransferListener {
+
+        // Simply updates the UI list when notified.
+        @Override
+        public void onError(int id, Exception e) {
+            Log.e(TAG, "Error during upload: " + id, e);
+        }
+
+        @Override
+        public void onProgressChanged(int id, long bytesCurrent, long bytesTotal) {
+            Log.d(TAG, String.format("onProgressChanged: %d, total: %d, current: %d",
+                    id, bytesTotal, bytesCurrent));
+        }
+
+        @Override
+        public void onStateChanged(int id, TransferState newState) {
+            Log.d(TAG, "onStateChanged: " + id + ", " + newState);
+        }
+    }
+
+    // Initialize the Amazon Cognito credentials provider
+    /*CognitoCachingCredentialsProvider credentialsProvider = new CognitoCachingCredentialsProvider(
+            getApplicationContext(),
+            "ap-south-1:3ebcfe7b-ef61-4ff4-9a67-ea9baa020a14", // Identity Pool ID
+            Regions.AP_SOUTH_1 // Region
+            /storage/emulated/0/DCIM/Screenshots/Screenshot_2017-04-12-09-23-03-002_com.android.chrome.png
+            /storage/emulated/0/DCIM/Screenshots/Screenshot_2017-05-19-22-42-17-926_com.linkedin.android.png
+
+
+    );*/
 }
